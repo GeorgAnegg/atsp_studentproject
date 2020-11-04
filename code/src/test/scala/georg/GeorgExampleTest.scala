564e5bd61c04cfa9ed6ad895a2669c4a76b97e73
Georg Anegg <georg.anegg@gmail.com>
2020-11-04 12:34:57 +0100

    Added basic sbt structure

---
+++ code/src/test/scala/georg/GeorgExampleTest.scala
@@ -0,0 +1,9 @@
+package georg
+
+import ch.ethz.math.ifor.atsp.Input
+
+object GeorgExampleTest extends App {
+  // execute this with 'test:run' in the sbt shell
+  val a = new Input
+  print("This is a test \n")
+}
---
+++ code/src/test/scala/yu/YuExampleTest.scala
@@ -0,0 +1,6 @@
+package yu
+
+object YuExampleTest extends App {
+  // execute this with 'test:run' in the sbt shell
+  print("This is a test \n")
+}
