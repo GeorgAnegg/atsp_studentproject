747f544aeab32b4e1940cba2049f09bb8a13c70a
Georg Anegg <georg.anegg@gmail.com>
2021-03-04 21:37:32 +0100

    added CSVs to resources and ImportTest to create input from them

--- code/src/main/scala/ch/ethz/math/ifor/atsp/dataProcessing/CSV.scala
+++ code/src/main/scala/ch/ethz/math/ifor/atsp/dataProcessing/CSV.scala
@@ -1,9 +1,51 @@
 package ch.ethz.math.ifor.atsp.dataProcessing
 
+import java.io.{FileNotFoundException, IOException}
+
 import ch.ethz.math.ifor.atsp.Input
+
+import scala.io.Source
 
 object CSV {
 
-  def createInput: Input = ???
+  def createInput(filename:String): Input = {
+    // open csv file
+    val bufferedSource = Source.fromResource(filename)
+    var distMatrix: Array[String] = Array()
+    var c = 0
+    //val iterableLine = bufferedSource.getLines
+    var numSites = 0
+    //println(numSites)
+    try {
+      for (line <- bufferedSource.getLines) {
+        var rows = line.split(";").map(_.trim)
+        //val rows_value = rows.tail
+        rows = rows.filter(_ != "")
+        distMatrix = distMatrix ++ rows
+        numSites += 1
+        println(rows.mkString("Array(", ", ", ")"))
+      }
+    } catch {
+      case e: FileNotFoundException => println("Couldn't find that file.")
+      case e: IOException => println("Got an IOException.")
+    }
+
+    var result_matrix: Array[Vector[Double]] = Array.ofDim[Vector[Double]](numSites)
+    for (value <- 0 until numSites){
+      val row = distMatrix.take(numSites)
+      val result_row = row.map(x => x.toDouble).toVector
+      result_matrix(value) = result_row
+      distMatrix = distMatrix.drop(numSites)
+    }
+    bufferedSource.close
+    /*
+    println(result_matrix.length,result_matrix(0).length)
+    for (i<-result_matrix){
+      println(i)
+    }
+
+     */
+    Input.fromDistVec(result_matrix.toVector)
+  }
 
 }
---
+++ code/src/test/scala/georg/ImportTest.scala
@@ -0,0 +1,37 @@
+package georg
+
+
+import ch.ethz.math.ifor.atsp.BranchAndBound.{BranchAndBoundSolver, BranchNode, rSAPLowerBoundSolver}
+import ch.ethz.math.ifor.atsp.dataProcessing.CSV
+
+object ImportTest extends App {
+  val filenames= List("br17.csv",
+    "ftv170.csv",
+    "ftv38.csv",
+    "ftv55.csv",
+    "kro124p.csv",
+    "rbg358.csv",
+    "ry48p.csv",
+    "ft53.csv",
+    "ftv33.csv",
+    "ftv44.csv",
+    "ftv64.csv",
+    "p43.csv",
+    "rbg403.csv",
+    "ft70.csv",
+    "ftv35.csv",
+    "ftv47.csv",
+    "ftv70.csv",
+    "rbg323.csv",
+    "rbg443.csv")
+
+  val input = CSV.createInput("br17.csv")
+  val t1 = System.nanoTime
+  val output = BranchAndBoundSolver.solve(input, "")
+  val duration = (System.nanoTime - t1) / 1e9d
+  output.print()
+
+  println("Run time:" + duration)
+
+
+}
