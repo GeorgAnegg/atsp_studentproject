10fef6c4048d2cbfd86c435acd85b2f11348b305
Georg Anegg <georg.anegg@gmail.com>
2021-03-09 12:23:48 +0100

    implemented new timeout

--- code/src/main/scala/ch/ethz/math/ifor/atsp/instanceAlgoMatrix/timeOut.scala
+++ code/src/main/scala/ch/ethz/math/ifor/atsp/instanceAlgoMatrix/timeOut.scala
@@ -2,11 +2,27 @@ package ch.ethz.math.ifor.atsp.instanceAlgoMatrix
 
 import ch.ethz.math.ifor.atsp.{Input, Output, Runtime}
 
+import scala.util.Try
 import scala.concurrent._
 import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
 
 object timeOut {
+
+  def custom_firstCompletedOf[T](futures: TraversableOnce[Future[T]]): Future[T] = {
+    val p = Promise[T]()
+    val pref = new java.util.concurrent.atomic.AtomicReference(p)
+    val completeFirst: Try[T] => Unit = { result: Try[T] =>
+      val promise = pref.getAndSet(null)
+      if (promise != null) {
+        promise.tryComplete(result)
+      }
+    }
+    futures foreach { _ onComplete completeFirst }
+    p.future
+  }
+
+
 
   /** runs solver and returns (optimal value, running time) or DNF if it timed out or ERROR if there was an error
     *
@@ -17,49 +33,46 @@ object timeOut {
     */
   def timed(maxTime:Int, input:Input, solver: Input=> Output ): Either[(Double,Runtime), String]= {
     try {
-      val timeout = maxTime.seconds
-      val temp = try {
-        val start = System.nanoTime
-        val output = Await.result(Future {
-          solver(input)
-        }, timeout)
-        (output.value, Runtime( (System.nanoTime - start) / 1e9d) )
-      }
-      catch {
-        case _: TimeoutException => {
-          return Right("DNF")
+      val fut = custom_firstCompletedOf(List(
+        Future {
+          val start = System.nanoTime
+          val output = solver(input)
+          val dur = Runtime((System.nanoTime - start) / 1e9d)
+          Left(output.value, dur)
+        },
+        Future {
+          Thread.sleep(maxTime * 1000)
+          Right("DNF")
         }
-      }
-      Left(temp)
+      )
+      )
+      Await.result(fut, Duration.Inf)
     }
+
     catch {
-      case _: Exception => {
-        Right("ERROR")
+      case e: Exception => Right("ERROR")
       }
     }
-  }
 
 
-
-
-
-
-
-/*
-
-  def timedToy(solver: Int=> Double, input:Int, maxTime:Int): Option[Double]= {
-    val timeout = maxTime.seconds
-    val temp = try {Await.result(Future{solver(input)},timeout)}
-    catch {case _:TimeoutException => {
-      println("Computation timed out!")
-      return None}
-    }
-    Some(temp)
-  }
-
-  def fun(x:Int, y:Int)= {Thread.sleep(6500)
-    x+0.5+10*y}
-
-  val res = timedToy(fun(_,0), 1, 5)
-*/
+//      val timeout = maxTime.seconds
+//      val temp = try {
+//        val start = System.nanoTime
+//        val output = Await.result(Future {
+//          solver(input)
+//        }, timeout)
+//        (output.value, Runtime( (System.nanoTime - start) / 1e9d) )
+//      }
+//      catch {
+//        case _: TimeoutException => {
+//          return Right("DNF")
+//        }
+//      }
+//      Left(temp)
+//    }
+//    catch {
+//      case _: Exception => {
+//        Right("ERROR")
+//      }
+//    }
 }
