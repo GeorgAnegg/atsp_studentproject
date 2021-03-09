55063f8f60e2ec87d529466dcb1b999118bf3ccb
Georg Anegg <georg.anegg@gmail.com>
2021-03-09 16:23:09 +0100

    implemented custom interruptable Future

--- code/src/main/scala/ch/ethz/math/ifor/atsp/instanceAlgoMatrix/timeOut.scala
+++ code/src/main/scala/ch/ethz/math/ifor/atsp/instanceAlgoMatrix/timeOut.scala
@@ -1,5 +1,7 @@
 package ch.ethz.math.ifor.atsp.instanceAlgoMatrix
 
+import java.util.concurrent.atomic.AtomicReference
+
 import ch.ethz.math.ifor.atsp.{Input, Output, Runtime}
 
 import scala.util.Try
@@ -9,6 +11,40 @@ import ExecutionContext.Implicits.global
 
 object timeOut {
 
+
+  /** returns a custom future that is interruptable
+    *
+    * @param fun
+    * @tparam T
+    * @return
+    */
+  def interruptableFuture[T](fun: () => T): (Future[T], () => Boolean) = {
+    val p = Promise[T]()
+    val f = p.future
+    val aref = new AtomicReference[Thread](null)
+    p tryCompleteWith Future {
+      val thread = Thread.currentThread
+      aref.synchronized { aref.set(thread) }
+      try fun() finally {
+        val wasInterrupted = aref.synchronized { aref getAndSet null }
+      }
+    }
+    (f, () => {
+      aref.synchronized { Option(aref getAndSet null) foreach { _.interrupt() } }
+      p.tryFailure(new CancellationException)
+    })
+  }
+
+
+
+
+
+  /** this is supposed to remove references to ongoing future and let it be garbage collected, but it didn't make a difference
+    *
+    * @param futures
+    * @tparam T
+    * @return
+    */
   def custom_firstCompletedOf[T](futures: TraversableOnce[Future[T]]): Future[T] = {
     val p = Promise[T]()
     val pref = new java.util.concurrent.atomic.AtomicReference(p)
@@ -24,6 +60,8 @@ object timeOut {
 
 
 
+
+
   /** runs solver and returns (optimal value, running time) or DNF if it timed out or ERROR if there was an error
     *
     * @param maxTime
@@ -31,6 +69,41 @@ object timeOut {
     * @param solver
     * @return
     */
+  def timed(maxTime:Int, input:Input , solver: Input=> Output): Either[(Double,Runtime), String]= {
+    try {
+
+      val (compute,cancel) = interruptableFuture[Either[(Double,Runtime), String]] { () =>
+        val start = System.nanoTime
+        val output = solver(input)
+        val dur = Runtime((System.nanoTime - start) / 1e9d)
+        Left(output.value, dur)
+      }
+      val sleep = Future{
+        Thread.sleep(maxTime*1000)
+        println("computation timed out")
+        cancel()
+        Right("DNF")}
+
+      val fut = Future.firstCompletedOf(List(
+        compute, sleep))
+
+      val temp = Await.result(fut, Duration.Inf)
+      temp
+    }
+
+    catch {
+      case e: Exception => {println(e)
+        Right("ERROR")
+      }
+    }
+  }
+
+
+
+
+
+
+  /*
   def timed(maxTime:Int, input:Input, solver: Input=> Output ): Either[(Double,Runtime), String]= {
     try {
       val fut = custom_firstCompletedOf(List(
@@ -53,7 +126,7 @@ object timeOut {
       case e: Exception => Right("ERROR")
       }
     }
-
+*/
 
 //      val timeout = maxTime.seconds
 //      val temp = try {
