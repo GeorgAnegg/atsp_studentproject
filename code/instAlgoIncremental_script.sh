91075215dee31db61cdb1cc318c320001b55603f
Georg Anegg <georg.anegg@gmail.com>
2021-03-17 15:38:14 +0100

    this works

--- code/instAlgoIncremental_script.sh
+++ code/instAlgoIncremental_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export SCALA_INTERPRETER=/u/ganegg/Downloads/scala-2.13.3/scala-2.13.3/bin/scala
-export SCALA_RUNTIME_LIBRARY=/u/ganegg/Downloads/scala-2.13.3/scala-2.13.3/lib/scala-library.jar
-export ASSEMBLY=/u/ganegg/Documents/atsp/code/target/scala-2.13/code-assembly-0.1.0-SNAPSHOT.jar
+export SCALA_INTERPRETER=/home/ganegg/tmp/scala-2.13.3/scala-2.13.3/bin/scala
+export SCALA_RUNTIME_LIBRARY=/home/ganegg/tmp/scala-2.13.3/scala-2.13.3/lib/scala-library.jar
+export ASSEMBLY=/home/ganegg/atsp/code/target/scala-2.13/code-assembly-0.1.0-SNAPSHOT.jar
 
 SOLVERS="CDT FT92 FT97 MTZ_FT97" # MTZ GG DL"
 INSTANCES="br17.csv ft70.csv" #ftv35.csv ftv47.csv  ftv70.csv    rbg323.csv  rbg443.csv  ftv170.csv  ftv38.csv  ftv55.csv  kro124p.csv  rbg358.csv ry48p.csv ft53.csv           ftv33.csv   ftv44.csv  ftv64.csv  p43.csv      rbg403.csv"
