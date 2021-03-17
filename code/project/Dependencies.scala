1c56b070bff7319fd6e9efbd3f16e340f36d5423
Georg Anegg <georg.anegg@gmail.com>
2021-03-17 15:53:27 +0100

    upgraded poi library

--- code/project/Dependencies.scala
+++ code/project/Dependencies.scala
@@ -6,9 +6,9 @@ object Dependencies {
   //lazy val sparkPoi = "org.apache.poi" % "poi" % "3.17" % "provided"
   //lazy val sparkSql = "org.apache.spark" %% "spark-sql" % "3.0.0" % "provided"
 
-  lazy val sparkPoi = "org.apache.poi" % "poi" % "3.15-beta2"
-  lazy val sparkPoiO = "org.apache.poi" % "poi-ooxml" % "3.15-beta2"
-  lazy val sparkPoiOS = "org.apache.poi" % "poi-ooxml-schemas" % "3.15-beta2"
+  lazy val sparkPoi = "org.apache.poi" % "poi" % "4.1.2"
+  lazy val sparkPoiO = "org.apache.poi" % "poi-ooxml" % "4.1.2"
+  lazy val sparkPoiOS = "org.apache.poi" % "poi-ooxml-schemas" % "4.1.2"
   lazy val optimus = "com.github.vagmcs" %% "optimus" % "3.2.4"
   lazy val optimusLP = "com.github.vagmcs" %% "optimus-solver-lp" % "3.2.4"
 }
