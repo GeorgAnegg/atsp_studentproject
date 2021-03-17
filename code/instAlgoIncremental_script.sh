339475805b19564a2d0d603581c35f28469cf22e
Georg Anegg <georg.anegg@gmail.com>
2021-03-17 15:03:13 +0100

    added progress to script

--- code/instAlgoIncremental_script.sh
+++ code/instAlgoIncremental_script.sh
@@ -5,7 +5,7 @@ export SCALA_RUNTIME_LIBRARY=/u/ganegg/Downloads/scala-2.13.3/scala-2.13.3/lib/s
 export ASSEMBLY=/u/ganegg/Documents/atsp/code/target/scala-2.13/code-assembly-0.1.0-SNAPSHOT.jar
 
 SOLVERS="CDT FT92 FT97 MTZ_FT97" # MTZ GG DL"
-INSTANCES="br17.csv" #ft70.csv ftv35.csv ftv47.csv  ftv70.csv    rbg323.csv  rbg443.csv  ftv170.csv  ftv38.csv  ftv55.csv  kro124p.csv  rbg358.csv ry48p.csv ft53.csv           ftv33.csv   ftv44.csv  ftv64.csv  p43.csv      rbg403.csv"
+INSTANCES="br17.csv ft70.csv" #ftv35.csv ftv47.csv  ftv70.csv    rbg323.csv  rbg443.csv  ftv170.csv  ftv38.csv  ftv55.csv  kro124p.csv  rbg358.csv ry48p.csv ft53.csv           ftv33.csv   ftv44.csv  ftv64.csv  p43.csv      rbg403.csv"
 
 # doesn't do anything JAVA_OPTS="-Djava.library.path=$OR_TOOLS_HOME/lib"
 
@@ -13,7 +13,8 @@ INSTANCES="br17.csv" #ft70.csv ftv35.csv ftv47.csv  ftv70.csv    rbg323.csv  rbg
 
 for instance in $INSTANCES; do
 	for solver in $SOLVERS; do
-		timeout 3 ./helper.sh $instance $solver
+		echo starting timeout on solving $instance with $solver
+		timeout 5 ./helper.sh $instance $solver
 		#sleep 3
 		#export solver
 		#export instance
