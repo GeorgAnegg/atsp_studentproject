81018b076548613384478563c88060ecc3de35b7
Georg Anegg <georg.anegg@gmail.com>
2021-03-19 23:27:38 +0100

    max heap size 8g

--- code/helper.sh
+++ code/helper.sh
@@ -4,4 +4,4 @@ export SCALA_INTERPRETER=/home/ganegg/tmp/scala-2.13.3/bin/scala
 export SCALA_RUNTIME_LIBRARY=/home/ganegg/tmp/scala-2.13.3/lib/scala-library.jar
 export ASSEMBLY=/home/ganegg/atsp/code/target/scala-2.13/code-assembly-0.1.0-SNAPSHOT.jar
 
-$SCALA_INTERPRETER -J-Djava.library.path=$OR_TOOLS_HOME/lib -J-Xmx4g -cp $ASSEMBLY:$SCALA_RUNTIME_LIBRARY instAlgoIncremental.scala $1 $2
+$SCALA_INTERPRETER -J-Djava.library.path=$OR_TOOLS_HOME/lib -J-Xmx8g -cp $ASSEMBLY:$SCALA_RUNTIME_LIBRARY instAlgoIncremental.scala $1 $2
