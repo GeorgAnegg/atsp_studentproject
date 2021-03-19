d0bcab93ca36d5d601a2bca16746bb183ce29620
Georg Anegg <georg.anegg@gmail.com>
2021-03-19 22:35:26 +0100

    fixed skip

--- code/instAlgoIncremental.scala
+++ code/instAlgoIncremental.scala
@@ -18,46 +18,67 @@ object instAlgoIncremental
     val rowNumber = 1+instances.indexOf(args(0).dropRight(4))
     val colNumber = 1+namedSolvers.map(_._1).indexOf(args(1))
 
-    val either =
-    try {
-      val input = CSV.createInput(args(0))
-      val start = System.nanoTime
-      
-      println(s"solving ${args(0).dropRight(4)} with solver ${args(1)}")
-
-      val output = namedSolvers.find(_._1 == args(1)).get._2(input)
-      val dur = Runtime((System.nanoTime - start) / 1e9d)
-      Left(output.value, dur)
-
-    }
-    catch {
-      case e: Any => {println(e)
-        Right("ERROR")}
-    }
-
     val filename = s"instAlgoMatrix_60.xlsx"
     val file = new FileInputStream(new File(filename))
 
     val workbook = new XSSFWorkbook(file)
     val sheetNames:List[String] = List("optValues", "runningTimes")
-//
-//    sheetNames.foreach { name =>
-//      val sheet = workbook.createSheet(name)
-//      //create header row
-//      val header = sheet.createRow(0)
-//      namedSolvers.map(_._1).zipWithIndex.foreach {
-//        case (solverID, index)=>
-//          val cell = header.createCell(index)
-//          cell.setCellValue(solverID)}
-//      instances.zipWithIndex.foreach {
-//        case (instanceID, index) =>
-//          val row = sheet.createRow(index + 1)
-//          val cell = row.createCell(0)
-//          cell.setCellValue(instanceID)
-//      }
-//    }
 
-    writeCell(rowNumber,colNumber,either,workbook, sheetNames)
+
+    sheetNames.foreach(category => {
+      val sheet = workbook.getSheet(category)
+      var row = sheet.getRow(rowNumber)
+      if (row ==null) {row = sheet.createRow(rowNumber) }
+
+
+      var cell = row.getCell(colNumber)
+      if (cell == null) {
+        cell = row.createCell(colNumber)
+        cell.setCellValue("empty")}
+      if (List("ERROR", "emtpy").contains(cell.getStringCellValue)) {
+
+        val either =
+          try {
+            val input = CSV.createInput(args(0))
+            val start = System.nanoTime
+
+            println(s"solving ${args(0).dropRight(4)} with solver ${args(1)}")
+
+            val output = namedSolvers.find(_._1 == args(1)).get._2(input)
+            val dur = Runtime((System.nanoTime - start) / 1e9d)
+            Left(output.value, dur)
+
+          }
+          catch {
+            case e: Any => {println(e)
+              Right("ERROR")}
+          }
+
+
+
+        cell.setCellValue(either match {
+          case Left(pair) => {
+            category match {
+              case "optValues" => pair._1.toString
+              case "runningTimes" => pair._2.toString
+            }
+          }
+          case Right(s) => s
+        })
+
+
+      }
+      else {println("cell already filled")}
+
+    }
+    )
+
+
+
+
+    //writeCell(rowNumber,colNumber,either,workbook, sheetNames)
+
+
 
     val filename_global = System.getProperty("user.dir")+s"/${filename}"
 
